91854e992e3f1c9d97b5ef1023e24b4726be10e4
ChandanaRD <chanrd1996@gmail.com>
2019-06-17 10:57:56 +0530

    Updates

---
+++ precache-manifest.f6fead7877d2f30ba8593863ad451d66.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd364a65ded840327b627b903675db1",
+    "url": "/all_about_friends_the_series/index.html"
+  },
+  {
+    "revision": "a694ccf2dcc1d0b4bfe9",
+    "url": "/all_about_friends_the_series/static/css/main.199be759.chunk.css"
+  },
+  {
+    "revision": "0bb4cfc44c5ecac7cf8e",
+    "url": "/all_about_friends_the_series/static/js/2.a08c5ae9.chunk.js"
+  },
+  {
+    "revision": "a694ccf2dcc1d0b4bfe9",
+    "url": "/all_about_friends_the_series/static/js/main.fdff2dcb.chunk.js"
+  },
+  {
+    "revision": "60fa3036a8897e20cd95",
+    "url": "/all_about_friends_the_series/static/js/runtime~main.c88ff643.js"
+  },
+  {
+    "revision": "7e4e6479c72bbd159bbbfa5eb1a348be",
+    "url": "/all_about_friends_the_series/static/media/ChandlerBing.7e4e6479.jpg"
+  },
+  {
+    "revision": "71df519173eaf8fb9c0934b6ec710fcc",
+    "url": "/all_about_friends_the_series/static/media/Friends-logo-wallpaper.71df5191.jpg"
+  },
+  {
+    "revision": "615bec348661164764339a440c4d3098",
+    "url": "/all_about_friends_the_series/static/media/JoeyTribbiani.615bec34.jpg"
+  },
+  {
+    "revision": "52364a297b8f265af554b4c810889236",
+    "url": "/all_about_friends_the_series/static/media/MonicaGeller.52364a29.jpg"
+  },
+  {
+    "revision": "7468e84fad9953b35b0f595064ff0b62",
+    "url": "/all_about_friends_the_series/static/media/PhoebeBuffay.7468e84f.jpg"
+  },
+  {
+    "revision": "46e336112077ff1462a7eafb15f81991",
+    "url": "/all_about_friends_the_series/static/media/RachelGreen.46e33611.jpg"
+  },
+  {
+    "revision": "bba5338eff3a574a86ce4e2cce29a185",
+    "url": "/all_about_friends_the_series/static/media/RossGeller.bba5338e.jpg"
+  }
+]);
